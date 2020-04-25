d7ee88466876407fe4c93d9d292168fba4536e7e
Atanu Bhowmick <mail2atanu007@gmail.com>
2020-04-25 18:03:20 +0530

    Config server updated with git url

--- src/main/java/com/atanu/spring/config/ConfigServerApplication.java
+++ src/main/java/com/atanu/spring/config/ConfigServerApplication.java
@@ -2,6 +2,7 @@ package com.atanu.spring.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @author Atanu Bhowmick
  *
  */
+@EnableConfigServer
 @EnableEurekaClient
 @SpringBootApplication
 public class ConfigServerApplication {
